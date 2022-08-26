e23fc38d6bf3a0216601faa3df4a9c686a4dbf0f
Martin Prinz <martin.prinz@maibornwolff.de>
2022-08-26 19:21:08 +0200

    add storybook turbo build

--- tlm-stencil/.storybook/main.js
+++ tlm-stencil/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-turbo-build'],
   framework: '@storybook/html',
 };
