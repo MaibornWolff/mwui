5ef1ee435988c24935e3d9687bfccc76c210da66
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-08-26 17:17:59 +0000

    gh actions: cache npm

--- tlm-stencil/dist/cjs/my-component_2.cjs.entry.js
+++ tlm-stencil/dist/cjs/my-component_2.cjs.entry.js
@@ -895,7 +895,7 @@ var _createEmotion = createEmotion({
 
 /**
  * Do not edit directly
- * Generated on Fri, 26 Aug 2022 14:18:04 GMT
+ * Generated on Fri, 26 Aug 2022 17:17:09 GMT
  */
 const xs = 8;
 const fontFamiliesDefault = "roboto";
