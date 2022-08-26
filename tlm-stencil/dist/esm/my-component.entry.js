62fb3bc166075ca010239301d185cbf284464c51
Martin Prinz <martin.prinz@maibornwolff.de>
2022-08-26 15:46:14 +0200

    remove tlm-stencil/docs and dist from .gitignore

---
+++ tlm-stencil/dist/esm/my-component.entry.js
@@ -0,0 +1,22 @@
+import { r as registerInstance, h } from './index-6618eff3.js';
+
+function format(first, middle, last) {
+  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
+}
+
+const myComponentCss = ":host{display:block}";
+
+const MyComponent = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  getText() {
+    return format(this.first, this.middle, this.last);
+  }
+  render() {
+    return h("div", null, "Hello, World! I'm ", this.getText());
+  }
+};
+MyComponent.style = myComponentCss;
+
+export { MyComponent as my_component };
