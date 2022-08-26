2463858a0b653c934742851be678fc04599baac0
Martin Prinz <martin.prinz@maibornwolff.de>
2022-08-26 13:11:51 +0000

    remove tlm-react as submodule

---
+++ tlm-react/README.md
@@ -0,0 +1,13 @@
+# The Liberation Machine - React
+
+## Step 1.
+
+- Build your core stencil package.
+
+## Step 2.
+
+- Run build on this package.
+
+## Step 3.
+
+- Publish.
---
+++ tlm-react/src/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'tlm-stencil';
+
+import { defineCustomElements } from 'tlm-stencil/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const TlmButton = /*@__PURE__*/createReactComponent<JSX.TlmButton, HTMLTlmButtonElement>('tlm-button');
