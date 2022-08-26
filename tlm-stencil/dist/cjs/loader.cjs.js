1a75dc71eeb7fc6e05987d31ab4880236f309f68
Martin Prinz <martin.prinz@maibornwolff.de>
2022-08-26 12:54:05 +0000

    add root level npm scripts

--- tlm-stencil/dist/cjs/loader.cjs.js
+++ tlm-stencil/dist/cjs/loader.cjs.js
@@ -2,19 +2,35 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-const index = require('./index-b50f3c9d.js');
+const index = require('./index-6c26ed16.js');
+const appGlobals = require('./app-globals-3a1e7e63.js');
 
 /*
  Stencil Client Patch Esm v2.17.3 | MIT Licensed | https://stenciljs.com
  */
 const patchEsm = () => {
+    // NOTE!! This fn cannot use async/await!
+    // @ts-ignore
+    if (index.BUILD.cssVarShim && !(index.CSS && index.CSS.supports && index.CSS.supports('color', 'var(--c)'))) {
+        // @ts-ignore
+        return Promise.resolve().then(function () { return require(/* webpackChunkName: "polyfills-css-shim" */ './css-shim-32724599.js'); }).then(() => {
+            if ((index.plt.$cssShim$ = index.win.__cssshim)) {
+                return index.plt.$cssShim$.i();
+            }
+            else {
+                // for better minification
+                return 0;
+            }
+        });
+    }
     return index.promiseResolve();
 };
 
 const defineCustomElements = (win, options) => {
   if (typeof window === 'undefined') return Promise.resolve();
   return patchEsm().then(() => {
-  return index.bootstrapLazy([["my-component_2.cjs",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}],[4,"tlm-button",{"testId":[1,"test-id"],"disabled":[4]}]]]], options);
+  appGlobals.globalScripts();
+  return index.bootstrapLazy([["my-component.cjs",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]],["tlm-button.cjs",[[4,"tlm-button",{"testId":[1,"test-id"],"disabled":[4]}]]]], options);
   });
 };
 
--- tlm-stencil/dist/components/index.js
+++ tlm-stencil/dist/components/index.js
@@ -1,3 +1,7 @@
 export { setAssetPath, setPlatformOptions } from '@stencil/core/internal/client';
 export { MyComponent, defineCustomElement as defineCustomElementMyComponent } from './my-component.js';
 export { TlmButton, defineCustomElement as defineCustomElementTlmButton } from './tlm-button.js';
+
+const globalScripts = () => {};
+
+globalScripts();
