d780492dac1f71d2cf5463eef178f5804f7a9d45
Fuasmattn <Fuasmattn@users.noreply.github.com>
2022-08-26 19:30:03 +0000

    fix link

--- tlm-stencil/tlm-token-farm/dist/js/global.js
+++ tlm-stencil/tlm-token-farm/dist/js/global.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 26 Aug 2022 19:27:35 GMT
+ * Generated on Fri, 26 Aug 2022 19:29:34 GMT
  */
 export const magenta100 = "#fde8f2";
 export const magenta200 = "#f9b9d7";
