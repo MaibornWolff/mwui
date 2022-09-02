5ccd0d6269fa92bce3bd90fb7cface03ba871041
Michael Buchholz <michael.j.buchholz@t-online.de>
2022-09-02 14:19:10 +0200

    add tlm-stencil to tlm-component-library dependencies, add module to public-api

--- tlm-angular/projects/tlm-component-library/src/public-api.ts
+++ tlm-angular/projects/tlm-component-library/src/public-api.ts
@@ -3,3 +3,6 @@
  */
 
 export { DIRECTIVES } from './lib/stencil-generated';
+export { MyComponent} from './lib/stencil-generated/components';
+export { TlmButton} from './lib/stencil-generated/components';
+export {TlmComponentLibraryModule} from './lib/tlm-component-library.module';
