066985a5bb47c4ac14ec59a83eb04eda3b35da93
Martin Prinz <martin.prinz@maibornwolff.de>
2022-10-06 16:57:53 +0200

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,13 +4,29 @@ npm publish
 
 cd ../tlm-angular
 npm i tlm-stencil@latest
-npm publish
+#npm publish
 
 cd ../tlm-vue
 npm i tlm-stencil@latest
-npm publish
+#npm publish
 
 cd ../tlm-react
 npm i tlm-stencil@latest
+#npm publish
+
+echo "Committing package.json updates."
+
+git add .
+git commit -m "bump tlm-stencil version after npm publish"
+git push
+
+echo "Publish component integrations"
+
+cd ../tlm-angular
 npm publish
 
+cd ../tlm-vue
+npm publish
+
+cd ../tlm-react
+npm publish
