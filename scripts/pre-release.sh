7e206454da8ed055503e066a87bc590585ac3bdf
Martin Prinz <martin.prinz@maibornwolff.de>
2022-10-12 14:16:07 +0200

    remove release scripts

--- scripts/pre-release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ./tlm-stencil
-npm publish
--- scripts/update-stencil.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cd ./tlm-angular && npm i tlm-stencil@latest && cd ../tlm-vue && npm i tlm-stencil@latest && cd ../tlm-react && npm i tlm-stencil@latest
