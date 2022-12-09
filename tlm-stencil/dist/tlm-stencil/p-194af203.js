6090a5427176f92a697bc6c64bc64600c8212795
Fuasmattn <mprinz90@gmail.com>
2022-12-09 21:49:18 +0000

    tlm-button: update build

---
+++ tlm-stencil/dist/tlm-stencil/p-194af203.js
@@ -0,0 +1,1 @@
+function i(i,t,l){return(i||"")+(t?` ${t}`:"")+(l?` ${l}`:"")}function t(i){return{light:300,regular:400,semibold:400,bold:700,italic:300,"light italic":300}[i.toLowerCase()]}export{i as f,t as g}
