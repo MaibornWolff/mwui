f2ed7033cc07c78661a59df6cb2bc2acf528651a
Michael Buchholz <michael.j.buchholz@t-online.de>
2022-12-15 18:37:32 +0100

    rewrote css to make it more like figma component

--- tlm-stencil/dist/tlm-stencil/mw-radio-button.entry.js
+++ tlm-stencil/dist/tlm-stencil/mw-radio-button.entry.js
@@ -1,13 +1,13 @@
 import { r as registerInstance, h, g as getElement } from './index-310f4825.js';
 
-const mwRadioButtonCss = ".container{display:block;position:relative;padding-left:35px;cursor:pointer;font-size:22px;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.container input{position:absolute;opacity:0;cursor:pointer;height:0;width:0;margin:0;top:0;left:0;border:none}.checkmark{position:absolute;top:0;left:0;height:25px;width:25px;background-color:#eee;border-radius:50%}.container:hover input~.checkmark{background-color:#ccc}.container:hover input:disabled~.checkmark{background-color:#eee}.container input:checked~.checkmark{background-color:#2196F3}.checkmark:after{content:\"\";position:absolute;display:none}.container input:checked~.checkmark:after{display:block}.container .checkmark:after{top:9px;left:9px;width:7px;height:7px;border-radius:50%;background:white}";
+const mwRadioButtonCss = ".container{display:flex;position:relative;cursor:pointer;align-items:center}.container input{display:none}.checkmark{box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;display:flex;justify-content:center;align-items:center;height:20px;width:20px;border:2px solid black;border-radius:50%;margin:14px}.container:hover input~.checkmark{background:#ff4c4c}.container:hover input:disabled~.checkmark{background:none}.container input:checked~.checkmark{border:6px solid red}.container.hover input:checked~.checkmark{border:6px solid #ff4c4c;background:none}";
 
 const MwRadioButton = class {
   constructor(hostRef) {
     registerInstance(this, hostRef);
   }
   render() {
-    return (h("label", { class: "container" }, h("input", { type: "radio", value: this.value, name: this.name, checked: this.checked, disabled: this.disabled }), h("span", { class: "checkmark" }, h("span", { class: "checkmark-dot" })), h("slot", null)));
+    return (h("label", { class: "container" }, h("input", { type: "radio", value: this.value, name: this.name, checked: this.checked, disabled: this.disabled }), h("span", { class: "checkmark" }), h("slot", null)));
   }
   get hostElement() { return getElement(this); }
 };
