653b0389e3ff12741b665a1fcc2652182d19a356
Fuasmattn <mprinz90@gmail.com>
2022-12-16 14:26:39 +0100

    use tokens in storybook theme

--- tlm-stencil/.storybook/theme.js
+++ tlm-stencil/.storybook/theme.js
@@ -1,5 +1,13 @@
 import { create } from '@storybook/theming';
 import logo from './public/mwui_logo.svg';
+import {
+  mwCoreColorSecondary900,
+  mwCoreColorSecondary500,
+  mwCoreColorPrimary500,
+  mwCoreFontFontFamiliesBody,
+  borderRadiusMedium,
+  mwCoreColorNeutral100,
+} from '../../tlm-token-farm/dist/js/MW_core';
 
 export default create({
   base: 'light',
@@ -8,25 +16,25 @@ export default create({
   brandImage: logo,
   brandTarget: '_self',
 
-  // colorPrimary: 'hotpink',
-  // colorSecondary: 'deepskyblue',
+  colorPrimary: mwCoreColorPrimary500,
+  colorSecondary: mwCoreColorSecondary900,
 
   // UI
-  // appBg: 'white',
+  appBg: mwCoreColorNeutral100,
   // appContentBg: 'white',
   // appBorderColor: 'grey',
-  // appBorderRadius: 4,
+  appBorderRadius: borderRadiusMedium,
 
   // Typography
-  // fontBase: '"Roboto", sans-serif',
+  fontBase: mwCoreFontFontFamiliesBody,
   // fontCode: 'monospace',
 
   // Text colors
-  // textColor: 'black',
-  // textInverseColor: 'rgba(255,255,255,0.9)',
+  // textColor: mwCoreColorNeutralWhite,
+  // textInverseColor: 'black',
 
   // Toolbar default and active colors
-  // barTextColor: 'white',
-  barSelectedColor: '#1c174a',
-  // barBg: 'lightgrey',
+  barTextColor: 'white',
+  barSelectedColor: mwCoreColorPrimary500,
+  barBg: mwCoreColorSecondary900,
 });
