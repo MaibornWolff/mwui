dc8e6829b5108037691a5ec02c03288e51abcdce
Fuasmattn <Fuasmattn@users.noreply.github.com>
2023-01-06 17:55:55 +0000

    fix button icon layout

--- tlm-stencil/tlm-token-farm/dist/js/MW_component.js
+++ tlm-stencil/tlm-token-farm/dist/js/MW_component.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Thu, 05 Jan 2023 17:27:06 GMT
+ * Generated on Fri, 06 Jan 2023 17:54:48 GMT
  */
 export const mwComponentButtonFilledColorBgDefault = "#ed2985";
 export const mwComponentButtonFilledColorBgHover = "#c42177";
