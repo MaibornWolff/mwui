b8c515c761ede7eb062178b6cfc3c4b9b8d18649
Fuasmattn <Fuasmattn@users.noreply.github.com>
2023-01-13 12:37:11 +0000

    fix react integration; version 0.1.8

--- mwui-stencil/mwui-token-farm/dist/js/MW_component.js
+++ mwui-stencil/mwui-token-farm/dist/js/MW_component.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 13 Jan 2023 12:14:54 GMT
+ * Generated on Fri, 13 Jan 2023 12:35:46 GMT
  */
 export const mwComponentButtonFilledColorBgDefault = "#ed2985";
 export const mwComponentButtonFilledColorBgHover = "#c42177";
