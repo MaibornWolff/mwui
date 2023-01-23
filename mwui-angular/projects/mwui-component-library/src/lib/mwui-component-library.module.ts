10adeda91f5652839a2974625ede10b63000a09b
Philipp Rummelsberger <philipp.rummelsberger@maibornwolff.de>
2023-01-23 15:45:08 +0100

    #7: fix angular integration

--- mwui-angular/projects/mwui-component-library/src/lib/mwui-component-library.module.ts
+++ mwui-angular/projects/mwui-component-library/src/lib/mwui-component-library.module.ts
@@ -1,9 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core"
 import { DIRECTIVES } from './stencil-generated';
+import { defineCustomElements } from "@stencil-library/loader"
+
+defineCustomElements(window);
+
+const DECLARATIONS = [
+  // generated proxies
+  ...DIRECTIVES,
+];
 
 @NgModule({
-  declarations: [...DIRECTIVES],
-  imports: [],
-  exports: [...DIRECTIVES],
+  declarations: DECLARATIONS,
+  exports: DECLARATIONS,
 })
 export class MwuiComponentLibraryModule {}
--- mwui-angular/projects/mwui-component-library/src/public-api.ts
+++ mwui-angular/projects/mwui-component-library/src/public-api.ts
@@ -3,4 +3,5 @@
  */
 
 export { DIRECTIVES } from './lib/stencil-generated';
+export * from './lib/stencil-generated/components';
 export {MwuiComponentLibraryModule} from './lib/mwui-component-library.module';
