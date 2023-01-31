5191251c40676c78fd7d24c2a416ab3eb36b79ad
Martin Prinz <martin.prinz@maibornwolff.de>
2023-01-31 12:00:04 +0100

    enable scss token variables build

--- mwui-token-farm/src/scripts/build.ts
+++ mwui-token-farm/src/scripts/build.ts
@@ -1,76 +1,87 @@
 /* eslint-disable no-console */
-const StyleDictionaryPackage = require('style-dictionary');
-const { createArray } = require('./utils.js');
-const formats = require('./formats.ts');
-const transforms = require('./transforms');
+const StyleDictionaryPackage = require("style-dictionary");
+const { createArray } = require("./utils.js");
+const formats = require("./formats.ts");
+const transforms = require("./transforms");
 
 formats.forEach(format => StyleDictionaryPackage.registerFormat(format));
 transforms.forEach(transform => StyleDictionaryPackage.registerTransform(transform));
 
-const baseTransforms = ['attribute/cti', 'size/px'];
-const jsTransforms = baseTransforms.concat(['name/cti/camel']);
-const scssTransforms = baseTransforms.concat(['name/cti/kebab']);
+const baseTransforms = ["attribute/cti", "size/px"];
+const jsTransforms = baseTransforms.concat(["name/cti/camel"]);
+const scssTransforms = baseTransforms.concat(["name/cti/kebab"]);
 
 const getStyleDictionaryConfig = (theme: string) => {
-  return {
-    source: [`input/${theme}.json`],
-    format: {
-      createArray,
-    },
-    platforms: {
-      css: {
-        transformGroup: 'css',
-        buildPath: 'dist/css/',
-        files: [
-          {
-            destination: `index.css`,
-            format: 'utilityClass',
-          },
-        ],
-      },
-      cssVariables: {
-        transformGroup: 'css',
-        buildPath: 'dist/css/',
-        files: [
-          {
-            destination: `${theme}.css`,
-            format: 'css/variables',
-          },
-        ],
-      },
-      ts: {
-        transformGroup: "js",
-        transforms: jsTransforms,
-        buildPath: 'dist/js/',
-        files: [
-          {
-            destination: `${theme}.js`,
-            format: 'javascript/es6',
-          },
-          {
-            destination: `${theme}.ts`,
-            format: 'javascript/es6',
-          },
-          {
-            destination: `${theme}.d.ts`,
-            format: "typescript/es6-declarations"
-          }
-        ]
-      },
-    },
-  };
+    return {
+        source: [`input/${theme}.json`],
+        format: {
+            createArray,
+        },
+        platforms: {
+            css: {
+                transformGroup: "css",
+                buildPath: "dist/css/",
+                files: [
+                    {
+                        destination: `index.css`,
+                        format: "utilityClass",
+                    },
+                ],
+            },
+            scss: {
+                transformGroup: "css",
+                transforms: scssTransforms,
+                buildPath: "dist/scss/",
+                files: [
+                    {
+                        destination: `${theme}.scss`,
+                        format: "scss/variables",
+                    },
+                ],
+            },
+            cssVariables: {
+                transformGroup: "css",
+                buildPath: "dist/css/",
+                files: [
+                    {
+                        destination: `${theme}.css`,
+                        format: "css/variables",
+                    },
+                ],
+            },
+            ts: {
+                transformGroup: "js",
+                transforms: jsTransforms,
+                buildPath: "dist/js/",
+                files: [
+                    {
+                        destination: `${theme}.js`,
+                        format: "javascript/es6",
+                    },
+                    {
+                        destination: `${theme}.ts`,
+                        format: "javascript/es6",
+                    },
+                    {
+                        destination: `${theme}.d.ts`,
+                        format: "typescript/es6-declarations",
+                    },
+                ],
+            },
+        },
+    };
 };
-console.log('Build started...');
+console.log("Build started...");
 
-['MW_core', 'MW_component', 'MW_semantic_light'].forEach(theme => {
-  console.log('\n==============================================');
-  console.log(`\nProcessing: [${theme}]`);
+["MW_core", "MW_component", "MW_semantic_light"].forEach(theme => {
+    console.log("\n==============================================");
+    console.log(`\nProcessing: [${theme}]`);
 
-  const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
-  StyleDictionary.buildAllPlatforms();
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+    StyleDictionary.buildAllPlatforms();
 
-  console.log('\nEnd processing');
+    console.log("\nEnd processing");
 });
 
-console.log('\n==============================================');
-console.log('\nBuild completed!');
+console.log("\n==============================================");
+console.log("\nBuild completed!");
