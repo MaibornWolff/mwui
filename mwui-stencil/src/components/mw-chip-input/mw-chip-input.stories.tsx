7a12bc0653cc5aeef02299c8afc334db9481060f
Philipp Rummelsberger <philipp.rummelsberger@maibornwolff.de>
2023-03-01 13:39:32 +0100

    fix(#42): fix inline padding for labels

--- mwui-stencil/src/components/mw-chip-input/mw-chip-input.stories.tsx
+++ mwui-stencil/src/components/mw-chip-input/mw-chip-input.stories.tsx
@@ -59,6 +59,31 @@ Disabled.args = {
   inline: false,
   required: false,
   disabled: false,
-  multiple: true,
-  selectedChips: "asdf",
+};
+
+const InlineTemplate = args => `
+  <mw-chip-input
+    selected-chips="${args.selectedChips}"
+    name="${args.name}"
+    label="${args.label}"
+    placeholder="${args.placeholder}"
+    helper-text="${args.helperText}"
+    has-error="${args.hasError}"
+    inline="${args.inline}"
+    required="${args.required}"
+    disabled="${args.disabled}"
+    ></mw-chip-input>
+`;
+
+export const Inline = InlineTemplate.bind({});
+Inline.args = {
+  label: "ChipInput",
+  value: "",
+  name: "chip-input",
+  placeholder: "Enter a value",
+  helperText: "",
+  hasError: false,
+  inline: true,
+  required: false,
+  disabled: false,
 };
