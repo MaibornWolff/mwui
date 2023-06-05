44e22c169079647f25e515abed4c46a0aab1714a
Martin Prinz <martin.prinz@maibornwolff.de>
2023-06-05 07:27:27 +0000

    update vue example

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,18 +1,15 @@
 <script>
-import { MwButton, MwCard, MwCardBody } from '@maibornwolff/mwui-vue';
+import { MwButton } from "@maibornwolff/mwui-vue";
 
 export default {
-  components: {
-    MwButton,
-    MwCard,
-    MwCardBody
-  },
+    components: {
+        MwButton,
+    },
 };
 </script>
 
 <template>
-  <div id="app">
-    <mw-button test-id="asdf" label="Button"></mw-button>
-    <mw-card><mw-card-body>Card Body</mw-card-body></mw-card>
-  </div>
+    <div id="app">
+        <mw-button test-id="asdf" label="Button"></mw-button>
+    </div>
 </template>
