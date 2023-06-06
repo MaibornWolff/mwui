4f12b090d653ebe9152a53e3cdca134fa18e4ac9
Martin Prinz <martin.prinz@maibornwolff.de>
2023-06-06 13:17:44 +0000

    chore: fix imports for es modules

--- mwui-react/src/components/stencil-generated/react-component-lib/index.js
+++ mwui-react/src/components/stencil-generated/react-component-lib/index.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var createComponent_1 = require("./createComponent");
-Object.defineProperty(exports, "createReactComponent", { enumerable: true, get: function () { return createComponent_1.createReactComponent; } });
-var createOverlayComponent_1 = require("./createOverlayComponent");
-Object.defineProperty(exports, "createOverlayComponent", { enumerable: true, get: function () { return createOverlayComponent_1.createOverlayComponent; } });
+var createComponent_1 = require("./createComponent.js");
+Object.defineProperty(exports, "createReactComponent", {
+    enumerable: true,
+    get: function () {
+        return createComponent_1.createReactComponent;
+    },
+});
+var createOverlayComponent_1 = require("./createOverlayComponent.js");
+Object.defineProperty(exports, "createOverlayComponent", {
+    enumerable: true,
+    get: function () {
+        return createOverlayComponent_1.createOverlayComponent;
+    },
+});
